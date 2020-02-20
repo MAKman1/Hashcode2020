5095d3aa36a67cd1a835e096942ffc169266f8a0
Muhammad Bilal Bin Khalid <48215073+Bilalbink@users.noreply.github.com>
2020-02-20 22:11:55 +0300

    Library sorted by signUpTime

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
 
@@ -32,7 +34,19 @@ public class Main {
 
     //Step 1.2
     public static void orderLibInBooksBySignUpTime(){
+        for (int i = 0; i < bookCount; i++) {
+            Collections.sort(books.get(bookCount).libs, new CustomComparator());
+        }
+    }
 
+    // Custom Comparator
+    public static class CustomComparator implements Comparator<Library> {
+        @Override
+        public int compare(Library o1, Library o2) {
+            Integer a = new Integer(o1.signUpTime);
+            Integer b = new Integer(o2.signUpTime);
+            return a.compareTo(b);
+        }
     }
 
     //Step 1.3
