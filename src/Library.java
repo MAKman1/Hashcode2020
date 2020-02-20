42a8fedb6a308c6e60c94e104f61a9fc103fa65e
Muhammad Arham Khan <muhammadarhamkhan@gmail.com>
2020-02-20 22:15:40 +0300

    added id to lib

--- src/Library.java
+++ src/Library.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class Library {
+    int id;
     int bookCount;
     int signUpTime;
     int processLimit;
@@ -9,7 +10,8 @@ public class Library {
 
     ArrayList<Book> books;
 
-    public Library( int bookCount, int signUpTime, int processLimit){
+    public Library( int id, int bookCount, int signUpTime, int processLimit){
+        this.id = id;
         this.bookCount = bookCount;
         this.signUpTime = signUpTime;
         this.processLimit = processLimit;
